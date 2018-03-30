b84f014bf1d9ec2e450de38e6672eb00f1bfddac
saul <saul@raneeli.com>
2018-03-30 14:11:00 +0800

    token -> dgas

--- source/javascript/config.js
+++ source/javascript/config.js
@@ -1,8 +1,8 @@
 $(".remove_admin").click((e) => {
     e.preventDefault();
     var self = e.currentTarget;
-    var token_id = $(self).data('token-id');
-    if (token_id) {
-        $("#remove_admin_form_" + token_id).submit();
+    var dgas_id = $(self).data('dgas-id');
+    if (dgas_id) {
+        $("#remove_admin_form_" + dgas_id).submit();
     }
 });
---
+++ sql/migrate_00000001.sql
@@ -0,0 +1,2 @@
+ALTER TABLE admins RENAME COLUMN token_id TO dgas_id;
+ALTER TABLE sessions RENAME COLUMN token_id TO dgas_id;
